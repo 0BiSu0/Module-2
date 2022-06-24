bd601c75e77070b56f476d676c7302801b84d07d
0Bisu0 <vinhtuonglephuc@gmail.com>
2022-06-24 20:48:24 +0700

    Update Thuat toan tim kiem

---
+++ src/ss12_thuat_toan_tim_kiem/bai_tap/cai_dat_thuat_toan_tim_kiem_nhi_phan_su_dung_de_quy/BinarySearch.java
@@ -0,0 +1,26 @@
+package ss12_thuat_toan_tim_kiem.bai_tap.cai_dat_thuat_toan_tim_kiem_nhi_phan_su_dung_de_quy;
+
+public class BinarySearch {
+    int binarySearch(int array[], int x, int low, int high) {
+        if (high >= low) {
+            int mid = low + (high - low) / 2;
+            if (array[mid] == x)
+                return mid;
+            if (array[mid] > x)
+                return binarySearch(array, x, low, mid - 1);
+            return binarySearch(array, x, mid + 1, high);
+        }
+        return -1;
+    }
+    public static void main(String args[]) {
+        BinarySearch ob = new BinarySearch();
+        int array[] = { 3, 4, 5, 6, 7, 8, 9 };
+        int n = array.length;
+        int x = 4;
+        int result = ob.binarySearch(array, x, 0, n - 1);
+        if (result == -1)
+            System.out.println("Not found");
+        else
+            System.out.println("Element found at index " + result);
+    }
+}
